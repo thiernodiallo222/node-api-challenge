32eab2d6baf51c31c697a9a461a7c86b9829d9d2
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-04-17 22:05:06 -0400

    completed project half way

---
+++ actions/actionRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const db = require("./data/lambda.db3");
+
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(express.json());   // auto format in json
+const projectRoutes = require('./actions/actionRouter');
+const actionRouters = require('./projects/projectRouter');
+// require('dotenv').config();
+// const welcome = require('./server');
 
-Sing along:
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
+app.use('/projects', projectRoutes);
+app.use('/actions', actionRouters);
+const port = process.env.PORT||5000
 
-Go code!
-*/
+app.listen(port, () => console.log(`Server is listening at: http://localhost:${port}`));
+
+
+
+
